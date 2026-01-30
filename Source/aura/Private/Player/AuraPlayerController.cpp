dc7f1635b819057c693f96e171d5df5619eef4ef
mario.musov <mario.musov@hit.zone>
2026-01-30 22:55:04 +0200

    2.14.HighlightEnemies

--- Source/aura/Private/Player/AuraPlayerController.cpp
+++ Source/aura/Private/Player/AuraPlayerController.cpp
@@ -4,11 +4,80 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
-
+#include "Runtime/ApplicationCore/Internal/GenericPlatform/CursorUtils.h"	
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	
+	CursorTrace();
+}
+
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+	
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+	
+	/**
+	 *Line trace from cursor. THere are several scenarios:
+	 * A. LastActor is null && ThisActor is null
+	 *		-Do nothing
+	 * B. LastActor is null && ThisActor is valid
+	 *		-Highlight ThisActor
+	 * C. LastActor is valid && ThisActor is null
+	 *		-UnHighlight LastActor
+	 * D. Both actors are vlid, but LastActor != ThisActor
+	 *		-UnHiglight LastActor, and Highlight ThisActor	
+	 * E. Both actors are valid, and are the same actor
+	 *		-Do nothing
+	 */
+	
+	
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			// Case B
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			// Case A - both are null, do nothing
+		}
+	}
+	else // LastActor is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			// Case C
+			LastActor->UnHighlightActor();
+		}
+		else // both actors are valid
+		{
+			if (LastActor != ThisActor)
+			{
+				// Case D
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				// Case E - do nothing
+				
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -54,3 +123,5 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
 }
+
+

7b90651c07b156942b7d159157d623f15803db21
mario.musov <mario.musov@hit.zone>
2026-01-30 05:41:38 +0200

    2.12.GameMode

---
+++ Source/aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Your Grandma's Kite
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Your Grandma's Kite
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
